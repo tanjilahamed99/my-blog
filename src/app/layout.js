3b9d03c6f7b08a98997d07a5cf55df89700b164e
Tanjil Ahamed <tanjil113355@gmail.com>
2024-02-07 20:25:27 +0600

    google tag manager code added

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,41 +14,32 @@ export default function RootLayout({ children }) {
     <html lang="en">
 
       <head>
-        <Script
+        {/* <Script
           id="adsbygoogle-init"
           strategy="afterInteractive"
           crossOrigin="anonymous"
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9673111961225622"
         />
 
-        {/* <script
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=GTM-KNJN8X37`}
-        /> */}
         <script
           async
           src={`https://www.googletagmanager.com/gtag/js?id=G-0NT3DV49TB`}
-        />
+        /> */}
 
-        {/* <script
+
+
+        <Script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=GTM-KNJN8X37`}
+        />
+        
+        <script
           dangerouslySetInnerHTML={{
             __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', 'GTM-KNJN8X37', {
-              page_path: window.location.pathname,
-            });
-          `,
-          }}
-        /> */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-0NT3DV49TB', {
               page_path: window.location.pathname,
             });
           `,
